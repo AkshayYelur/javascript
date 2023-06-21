ad266335bb4caa6259c1c453805f955dc8ab3f8a
AkshayYelur <72262127+AkshayYelur@users.noreply.github.com>
2023-06-21 06:57:24 +0000

    datatypes done

---
+++ basics/02_datatypes.js
@@ -0,0 +1,11 @@
+let name ="Akshay"
+let age=23
+let isLoggedIn=false
+
+// number => 2 to power 53
+// bigint
+// String=""
+// boolean=> true/false
+// undefined=>
+// symbol=>unique
+ console.log(typeof age);
---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 145524
+let accountEmail = "ravi@gmail.com"
+var accountPassword = "1234"
+accountCity = "SOlapur"
+// accountId=2clear
+accountEmail="a@gmail.com"
+accountCity="pune"
+let accountState;
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+/*
+preper use not to var because of isuue in block scop and functional scop
+*/
