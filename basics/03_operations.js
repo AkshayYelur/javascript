5212b526fd609aa9d08fcddefda841e4c47987c8
AkshayYelur <72262127+AkshayYelur@users.noreply.github.com>
2023-06-23 06:57:30 +0000

    operation and comparision operation done

---
+++ basics/03_operations.js
@@ -0,0 +1,40 @@
+let somnumber =33
+let StringNumber = String(somnumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
+
+**************Operations************
+let value =3
+let negvalue = -value
+console.log(negvalue);
+
+let st="hello"
+let st2=" world"
+let st3 = st+st2
+console.log(st3);
+
+console.log("1"+2);
+
+console.log(1+"2");
+
+console.log(1+2+"2");
+
+console.log((3+4)*5%3);
+
+console.log(true);
+
+// **************************operations************************************
+
+console.log("2">1);
+console.log(null>1);
+console.log(null==0);
+console.log(num<=1);
+console.log(null>=1);
+
+
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
